935f59a73d274fdd871aff84fc48f9897f508381
MyoThuKha <myothukha.dev@gmail.com>
2022-12-02 15:45:59 +0630

    added smooth indicator

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:intro_ui/intros/page1.dart';
 import 'package:intro_ui/intros/page2.dart';
 import 'package:intro_ui/intros/page3.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 void main() {
   runApp(
@@ -19,14 +20,51 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     final controller = PageController();
 
-    return PageView(
-      controller: controller,
-      scrollDirection: Axis.horizontal,
-      children: const [
-        Page1(),
-        Page2(),
-        Page3(),
-      ],
+    return Scaffold(
+      body: Stack(
+        children: [
+          PageView(
+            controller: controller,
+            scrollDirection: Axis.horizontal,
+            children: const [
+              Page1(),
+              Page2(),
+              Page3(),
+            ],
+          ),
+          Container(
+            alignment: const Alignment(0, 0.8),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                TextButton(
+                  onPressed: () => controller.jumpToPage(2),
+                  child: const Text(
+                    "skip",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+                SmoothPageIndicator(
+                  controller: controller,
+                  count: 3,
+                  effect: const ExpandingDotsEffect(
+                    dotWidth: 14,
+                  ),
+                ),
+                TextButton(
+                  onPressed: () => controller.nextPage(
+                      duration: const Duration(milliseconds: 300),
+                      curve: Curves.easeIn),
+                  child: const Text(
+                    "next",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 }
